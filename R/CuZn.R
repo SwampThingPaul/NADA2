a592bc41868ab0cfdd8dd6add67c7809a51af66c
Paul Julian <pjulian@ufl.edu>
2025-08-08 12:40:35 -0400

    push updated vignette and added data

---
+++ R/CuZn.R
@@ -0,0 +1,20 @@
+#' CuZn
+#'
+#' @description
+#' Originally in the `NADA` R-Package.
+#'
+#' Copper and zinc concentrations in ground waters from two zones in the San Joaquin Valley of California.  The zinc concentrations were used.
+#'
+#' Objective is to determine if zinc concentrations differ between the two zones.  Zinc has two detection limits, at 3 and 10 ug/L.  Used in Chapters 4, 5 and 9 of the NADA book.
+#'
+#' @usage data(CuZn)
+#' @aliases CuZn
+#'
+#' @docType data
+#' @keywords dataset
+#' @name CuZn
+#' @source Millard and Deverel, 1988, Water Resources Research 24, pp.2087-2098.
+#' @references
+#' Helsel, D.R., 2005.Nondectects and Data Analysis; Statistics for censored environmental data. John Wiley & Sons, USA, N.J.
+
+"CuZn"
---
+++ R/ShePyrene.R
@@ -0,0 +1,20 @@
+#' ShePyrene
+#'
+#' @description
+#' Originally in the `NADA` R-Package.
+#'
+#' Pyrene concentrations in milligrams per liter from 20 water-quality monitoring stations in the Puget Sound of Washington State, USA.
+#'
+#' Used for characterizing priority pollutant concentrations in sediments of Puget Sound by computing summary statisitics.  Contains eight detection limits with 11 nondetects out of 56 total measurements.
+#'
+#' @usage data(ShePyrene)
+#' @aliases ShePyrene
+#'
+#' @docType data
+#' @keywords dataset
+#' @name ShePyrene
+#' @source She, N., 1997, Analyzing censored water quality data using a nonparametric approach.  Journal of the American Water Resources Association, 33, pp615--624.
+#' @references
+#' Helsel, D.R., 2005.Nondectects and Data Analysis; Statistics for censored environmental data. John Wiley & Sons, USA, N.J.
+
+"ShePyrene"
